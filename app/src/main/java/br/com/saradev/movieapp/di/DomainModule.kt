1dbc6fbb5b216326225b1bd6973f451a6ebb476f
saracavalcante <designersaracavalcante@gmail.com>
2022-05-16 15:11:28 -0300

    Add usecase layer

--- app/src/main/java/br/com/saradev/movieapp/di/DomainModule.kt
+++ app/src/main/java/br/com/saradev/movieapp/di/DomainModule.kt
@@ -3,14 +3,22 @@ package br.com.saradev.movieapp.di
 import br.com.saradev.movieapp.data_remote.repository.MovieRepositoryImpl
 import br.com.saradev.movieapp.data_remote.service.MovieService
 import br.com.saradev.movieapp.domain.repository.MovieRepository
+import br.com.saradev.movieapp.domain.usecase.GetPopularMoviesUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object DomainModule {
+
+    @Provides
+    @Singleton
+    fun provideGetPopularMoviesUseCase(repository: MovieRepository): GetPopularMoviesUseCase {
+        return GetPopularMoviesUseCase(repository)
+    }
 
     @Provides
     fun provideGetPopularMoviesRepo(
--- app/src/main/java/br/com/saradev/movieapp/domain/usecase/GetPopularMoviesUseCase.kt
+++ app/src/main/java/br/com/saradev/movieapp/domain/usecase/GetPopularMoviesUseCase.kt
@@ -1,22 +1,16 @@
 package br.com.saradev.movieapp.domain.usecase
 
-import br.com.saradev.movieapp.domain.model.Movie
+import br.com.saradev.movieapp.data_remote.model.MovieResponse
 import br.com.saradev.movieapp.domain.repository.MovieRepository
 import br.com.saradev.movieapp.utils.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetPopularMoviesUseCase @Inject constructor(
     private val movieRepository: MovieRepository,
 ) {
 
-    operator fun invoke(): Flow<Resource<List<Movie>>> = flow {
-        emit(Resource.Loading(""))
-        try {
-            emit(Resource.Sucess(movieRepository.getPopularMovies()))
-        } catch (e: Exception) {
-            emit(Resource.Error(e.message))
-        }
+    suspend operator fun invoke(apiKey: String): Flow<Resource<MovieResponse>> {
+        return movieRepository.getPopularMovies(apiKey)
     }
 }
