ed42478f1d974243d2286c803ad6782c157783c4
saracavalcante <designersaracavalcante@gmail.com>
2022-05-16 16:00:12 -0300

    Update movie repository

--- app/src/main/java/br/com/saradev/movieapp/data_remote/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/br/com/saradev/movieapp/data_remote/repository/MovieRepositoryImpl.kt
@@ -1,18 +1,32 @@
 package br.com.saradev.movieapp.data_remote.repository
 
+import br.com.saradev.movieapp.data_remote.model.MovieResponse
 import br.com.saradev.movieapp.data_remote.service.MovieService
-import br.com.saradev.movieapp.domain.mappers.toDomain
-import br.com.saradev.movieapp.domain.model.Movie
 import br.com.saradev.movieapp.domain.repository.MovieRepository
-import br.com.saradev.movieapp.utils.SafeApiRequest
+import br.com.saradev.movieapp.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
 import javax.inject.Inject
 
 class MovieRepositoryImpl @Inject constructor(
     private val movieService: MovieService,
-) : MovieRepository, SafeApiRequest() {
+) : MovieRepository {
 
-    override suspend fun getPopularMovies(): List<Movie> {
-        val response = safeApiRequest { movieService.getPopularMovies() }
-        return response.results?.toDomain()!!
+    override suspend fun getPopularMovies(apiKey: String): Flow<Resource<MovieResponse>> = flow {
+        try {
+            val result = movieService.getPopularMovies(apiKey = apiKey)
+            if (result.results.isEmpty()) {
+                emit(Resource.empty<MovieResponse>())
+            } else {
+                emit(Resource.success(result))
+            }
+        } catch (e: Exception) {
+            if (e is IOException) {
+                emit(Resource.error("No Internet Connection", null))
+            } else {
+                emit(Resource.error("Something went wrong...", null))
+            }
+        }
     }
 }
--- app/src/main/java/br/com/saradev/movieapp/domain/repository/MovieRepository.kt
+++ app/src/main/java/br/com/saradev/movieapp/domain/repository/MovieRepository.kt
@@ -1,7 +1,9 @@
 package br.com.saradev.movieapp.domain.repository
 
-import br.com.saradev.movieapp.domain.model.Movie
+import br.com.saradev.movieapp.data_remote.model.MovieResponse
+import br.com.saradev.movieapp.utils.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
-    suspend fun getPopularMovies(): List<Movie>
+    suspend fun getPopularMovies(apiKey: String): Flow<Resource<MovieResponse>>
 }
